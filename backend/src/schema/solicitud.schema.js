eae10079d0e16e5462925771b50bc71ce4a2e354
Ellorox <pablosantoschavez2001@gmail.com>
2023-10-25 23:09:19 -0300

    Modificando el scheme

--- backend/src/schema/solicitud.schema.js
+++ backend/src/schema/solicitud.schema.js
@@ -1,58 +1,40 @@
 "use strict";
 const Joi = require("joi");
-const ROLES = require("../constants/roles.constants");
 
-//  Esquema de validación para el cuerpo de la solicitud.
-const userBodySchema = Joi.object({
-  username: Joi.string().required().messages({
-    "string.empty": "El nombre de usuario no puede estar vacío.",
-    "any.required": "El nombre de usuario es obligatorio.",
-    "string.base": "El nombre de usuario debe ser de tipo string.",
-  }),
-  password: Joi.string().required().min(5).messages({
-    "string.empty": "La contraseña no puede estar vacía.",
-    "any.required": "La contraseña es obligatoria.",
-    "string.base": "La contraseña debe ser de tipo string.",
-    "string.min": "La contraseña debe tener al menos 5 caracteres.",
-  }),
-  email: Joi.string().email().required().messages({
-    "string.empty": "El email no puede estar vacío.",
-    "any.required": "El email es obligatorio.",
-    "string.base": "El email debe ser de tipo string.",
-    "string.email": "El email debe tener un formato válido.",
-  }),
-  roles: Joi.array()
-    .items(Joi.string().valid(...ROLES))
+const solicitudBodySchema = Joi.object({
+  userId: Joi.string()
     .required()
+    .pattern(/^(?:[0-9a-fA-F]{24}|[0-9a-fA-F]{12})$/)
     .messages({
-      "array.base": "El rol debe ser de tipo array.",
-      "any.required": "El rol es obligatorio.",
-      "string.base": "El rol debe ser de tipo string.",
-      "any.only": "El rol proporcionado no es válido.",
+      "string.empty": "El ID del usuario no puede estar vacío.",
+      "any.required": "El ID del usuario es obligatorio.",
+      "string.base": "El ID del usuario debe ser de tipo string.",
+      "string.pattern.base": "El ID proporcionado no es un ObjectId válido.",
     }),
-  newPassword: Joi.string().min(5).messages({
-    "string.empty": "La contraseña no puede estar vacía.",
-    "string.base": "La contraseña debe ser de tipo string.",
-    "string.min": "La contraseña debe tener al menos 5 caracteres.",
-  }),
-}).messages({
-  "object.unknown": "No se permiten propiedades adicionales.",
+  fechaCreacion: Joi.date()
+    .default(Date.now)
+    .messages({
+      "date.base": "La fecha de creación debe ser de tipo Date.",
+    }),
+  estado: Joi.string()
+    .valid("pendiente", "aprobada", "rechazada", "fuera de plazo")
+    .default("pendiente")
+    .messages({
+      "any.only": "El estado proporcionado no es válido.",
+    }),
 });
 
-/**
- * Esquema de validación para el id de usuario.
- * @constant {Object}
- */
-const userIdSchema = Joi.object({
+const solicitudIdSchema = Joi.object({
   id: Joi.string()
     .required()
     .pattern(/^(?:[0-9a-fA-F]{24}|[0-9a-fA-F]{12})$/)
     .messages({
-      "string.empty": "El id no puede estar vacío.",
-      "any.required": "El id es obligatorio.",
-      "string.base": "El id debe ser de tipo string.",
-      "string.pattern.base": "El id proporcionado no es un ObjectId válido.",
+      "string.empty": "El ID de la solicitud no puede estar vacío.",
+      "any.required": "El ID de la solicitud es obligatorio.",
+      "string.base": "El ID de la solicitud debe ser de tipo string.",
+      "string.pattern.base": "El ID proporcionado no es un ObjectId válido.",
     }),
 });
 
-module.exports = { userBodySchema, userIdSchema };
+
+module.exports = {solicitudBodySchema,solicitudIdSchema,};
