923a3835f482132dec2edc63aceabf69e9a9ef79
bastian <bastian.baeza1801@alumnos.ubiobio.cl>
2023-10-29 14:47:24 -0300

    Rama Actualizada

--- backend/src/routes/horario.routes.js
+++ backend/src/routes/horario.routes.js
@@ -1,18 +1,32 @@
 "use strict";
 const express = require("express");
-const router = express.Router();
+
 const horarioController = require("../controllers/horario.controller.js");
-const authMiddleware = require("../middlewares/authorization.middleware");
+const authMiddleware = require("../middlewares/authorization.middleware.js");
 const authenticationMiddleware = require("../middlewares/authentication.middleware.js");
 
+const router = express.Router();
+
+
+// Define el middleware de autenticación para todas las rutas
+router.use(authenticationMiddleware);
 
 //Define las rutas para los horarios
-router.post("/", horarioController.createHorario);
 router.get("/", horarioController.getHorarios);
 router.get("/:horarioId", horarioController.getHorarioById);
 router.put("/:horarioId",horarioController.updateHorarioById);
-router.delete("/:horarioId",horarioController.deleteHorarioById);
 
+//rutas que necesitan autenticacion y autorizacion de especialista
+
+router.post("/",authMiddleware.isEspecialista, horarioController.createHorario);
+router.delete("/:horarioId",authMiddleware.isEspecialista, horarioController.deleteHorarioById);
+
+
+
+/**
+*router.post("/",authMiddleware.isEspecialista, horarioController.createHorario);
+*router.delete("/:horarioId",authMiddleware.isEspecialista,horarioController.deleteHorarioById);
+*/
 
 
 module.exports = router;
