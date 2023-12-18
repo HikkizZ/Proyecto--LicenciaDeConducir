9f089ab3536b8acc7549b45ce8a083e954e63fa1
Hikki <fmiranda.reb@gmail.com>
2023-12-17 23:29:30 -0300

    Se corrige pequeño error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom/client';
-import App from './routes/App.jsx';
-import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Root from './routes/Root.jsx';
+import ActualizarCita from './components/citas/ActualizarCita.jsx';
+import CrearCita from './components/citas/CrearCita.jsx'; // Importa CrearCita
+import DetalleCita from './components/citas/DetalleCita.jsx'; // Importa DetalleCita
+import ListaCitas from './components/citas/ListaCitas.jsx'; // Importa ListaCitas
+import SubirArchivo from './components/upload/SubirArchivo.jsx'; // Importa SubirArchivo
+import './index.css';
+import App from './routes/App.jsx';
 import ErrorPage from './routes/ErrorPage.jsx';
 import Login from './routes/Login.jsx';
-import CrearCita from './components/citas/CrearCita.jsx'; // Importa CrearCita
-import ListaCitas from './components/citas/ListaCitas.jsx'; // Importa ListaCitas
-import DetalleCita from './components/citas/DetalleCita.jsx'; // Importa DetalleCita
-import ActualizarCita from './components/citas/ActualizarCita.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Root from './routes/Root.jsx';
 
 const router = createBrowserRouter([
   {
@@ -36,6 +37,10 @@ const router = createBrowserRouter([
       {
         path: '/actualizar-cita/:id', // Ruta para Actualizar Cita
         element: <ActualizarCita />,
+      },
+      {
+        path: '/subir-archivo', // Nueva ruta para subir archivo
+        element: <SubirArchivo />,
       },
     ],
   },
